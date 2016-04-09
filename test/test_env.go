f008bf3aaa8fcd1ca9bbbbe92f1f2b2149c6c845
NGHIEP <nghiepnds@yahoo.com>
2016-04-09 01:32:57 -0700

    Add test file

---
+++ test/test_env.go
@@ -0,0 +1,25 @@
+package main
+import (
+	"os"
+	"fmt"
+	"strings"
+	"utils/env"
+)
+
+func main() {
+	//To set a key/value pair, use os.Setenv.
+	os.Setenv("FOO", "1")
+	fmt.Println("FOO:", os.Getenv("FOO"))
+	fmt.Println("BAR:", os.Getenv("BAR"))
+
+	// Use os.Environ to list all key/value pairs in the environment.
+	// This returns a slice of strings in the form KEY=value.
+	// You can strings.Split them to get the key and value.
+	// Here we print all the keys.
+	fmt.Println()
+	for _, e := range os.Environ() {
+		pair := strings.Split(e, "=")
+		fmt.Println(pair[0], " : " , pair[1])
+	}
+	fmt.Println("Project path: ", env.GetProjectRoot())
+}
