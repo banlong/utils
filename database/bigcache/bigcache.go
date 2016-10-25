3015397cc387c05af7eb1acb92a7f31c4cecaeb1
banlong <nghiepnds@yahoo.com>
2016-10-25 11:30:28 -0700

    Add Bleve Indexing for database

--- database/bigcache/bigcache.go
+++ database/bigcache/bigcache.go
@@ -1,5 +1,5 @@
 //Example, bigcache object cannot be save. Data is on Heap, after app turn off the data will be erased
-package maind
+package main
 
 import (
 	"github.com/allegro/bigcache"
@@ -21,14 +21,23 @@ type Dog struct{
 // BigCache keeps entries on heap but omits GC for them. To achieve that operations on bytes arrays take place,
 // therefore entries (de)serialization in front of the cache will be needed in most use cases.
 func main()  {
+	BigCacheExample()
+}
 
+
+
+func BigCacheExample()  {
 	//bigcache provide only get and set
+	fmt.Println("STORE {STRING, STRING}")
 	cache, _ := bigcache.NewBigCache(bigcache.DefaultConfig(10 * time.Minute))
 	cache.Set("my-unique-key", []byte("value"))
 	entry, _ := cache.Get("my-unique-key")
-	fmt.Println("String value:", string(entry))
+	fmt.Println("Key:", "my-unique-key")
+	fmt.Println("Value:", string(entry))
+	fmt.Println();
 
 	//--------Store struct using json--------
+	fmt.Println("STORE {STRING, STRUCT}")
 	dog := Dog{
 		Name: "Nancy",
 		Owner: "Richard",
@@ -39,11 +48,14 @@ func main()  {
 	}
 	cache.Set("mydog", encodedDog)
 
-	//Get struct
+	//Get struct using Json
 	var getDog = new(Dog)
-	entry, _ = cache.Get("mydog")
+	key:= "mydog"
+	entry, _ = cache.Get(key)
 	err = json.Unmarshal(entry, getDog)
-	fmt.Println("Struct Value:",*getDog)
+	fmt.Println("Key:",key)
+	fmt.Println("Value:",*getDog)
+	fmt.Println();
 
 	//--------Store struct using GOB--------
 	doggy := Dog{
@@ -57,17 +69,57 @@ func main()  {
 	if err != nil{
 		log.Println(err.Error())
 	}
-	cache.Set("mydog", encodedDog2.Bytes())
+	key = "mydog"
+	cache.Set(key, encodedDog2.Bytes())
 
 	//Get struct
 	getDog = new(Dog)
-	entry, _ = cache.Get("mydog")
+	entry, _ = cache.Get(key)
 	decoder := gob.NewDecoder(bytes.NewReader(entry))
 	err = decoder.Decode(getDog)
-	fmt.Println("Struct Value:",*getDog)
+	fmt.Println("Key:",key)
+	fmt.Println("Value:",*getDog)
+	fmt.Println();
 
 
+	//--------Store struct using GOB--------
+	fmt.Println("STORE {STRUCT, STRUCT}")
+	sample3 := Dog{
+		Name: "Kitty",
+		Owner: "Bobby2",
+	}
+
+	encodeVal3 := new(bytes.Buffer)
+	encoder3 := gob.NewEncoder(encodeVal3)
+	err = encoder3.Encode(sample3)
+	if err != nil{
+		log.Println(err.Error())
+	}
+
+	inputKey := Key{
+		Id: "mydog",
+		Dept: "it",
+	}
+	keyStr, err := json.Marshal(inputKey)
+	cache.Set(string(keyStr), encodeVal3.Bytes())	//Bigcache only accept the string as key
+
+	//Get struct
+	searchKey := Key{
+		Id: "mydog",
+		Dept: "it",
+	}
+	searchKeyBytes, _ := json.Marshal(searchKey)
+	searchKeyStr := string(searchKeyBytes)
+	returnVal3 := new(Dog)
+	returnEncodedVal3, _ := cache.Get(searchKeyStr)
+	decoder3 := gob.NewDecoder(bytes.NewReader(returnEncodedVal3))
+	err = decoder3.Decode(returnVal3)
+	fmt.Println("Key:",searchKeyStr)
+	fmt.Println("Value:",*returnVal3)
+	fmt.Println();
 }
 
-
-
+type Key struct{
+	Id string
+	Dept string
+}
